bad1a2957c0b817b49770c0cc3918c306be0f308
Jose Hurtado <hello@hurtadojose.com>
2022-06-22 02:42:57 +0000

    Compiled 6.5

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,12 +1,9 @@
+// Already Compiled
+
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
-//your code here
 
-var initialValue = 0;
-var stopValue = 11;
-var increasingValue = -1;
-
-for(var i = initialValue; i<stopValue; i = i + increasingValue)
+for(var i = mySampleArray.length-1 ; i > -1; i = i-1)
 {
-	console.log(mySampleArray[i]);
-}
+    console.log(mySampleArray[i]);
+} 
