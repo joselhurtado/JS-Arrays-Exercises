8b4ca069dd004f5c03428f0ef9bbab44929b8621
Jose Hurtado <hello@hurtadojose.com>
2022-06-27 16:00:16 +0000

    Exercise 7.3 Done

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,12 @@
-var arr = [45,67,87,23,5,32,60];
+// Code Already Compiled
+let arr = [45, 67, 87, 23, 5, 32, 60];
 
 //Your code here.
+function reverse(b) {
+    let finalArray = [];
+    for (let i = b.length-1; i >= 0; i--) {
+        finalArray.push(b[i]);
+    }
+    return finalArray;
+}
+console.log(reverse(arr));
