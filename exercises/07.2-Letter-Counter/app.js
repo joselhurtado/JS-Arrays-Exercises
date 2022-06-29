48dda7aa98e1a89f16f4c495de503a4290399bc8
Jose Hurtado <hello@hurtadojose.com>
2022-06-29 03:47:53 +0000

    WIP Ecercise 7.2

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,26 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
 
-// your code here
+let parCounts = par.split(""); // Split String by Characters
 
-console.log(counts);
+function CharCounts();{
+for(let i = 0; i < parCounts.length; i++){
+    if(counts[parCounts[i]] == undefined)
+    counts[parCounts[i]] = 0;
+    counts[parCounts[i]]++;}
+}
+console.log(CharCounts());
+
+
+
+/* Another way to do this
+
+function count(par) {
+    var count = {};
+    par.split('').forEach(function (s) {
+        count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
+}
+
+console.log(count(par)); */
