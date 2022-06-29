05489e87915f270a24708666d03608f1b12a4b19
Jose Hurtado <hello@hurtadojose.com>
2022-06-29 04:04:41 +0000

    Exercise 7.2 for revision

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,7 +1,7 @@
+// My Way to Solve the Exercise
+
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
-
-// My Way to Solve the Exercise
 
 let parCounts = par.split(""); // Split String by Characters
 
