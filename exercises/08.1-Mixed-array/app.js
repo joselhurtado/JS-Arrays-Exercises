be4ea634cb3f694b090600b47cde25d07deb625d
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 16:00:12 +0000

    Exercise 8.2 Ongoing

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -6,7 +6,7 @@ var mix = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 let hello = {};
 hello = typeof(hello);
 
-let mixed = [typeof (mix[0]), ' ' + typeof (mix[1]), ' ' + typeof (mix[2]), ' ' + typeof (mix[3]), ' ' + typeof (mix[4]), ' ' + typeof (mix[5]), hello];
+let mixed = [typeof(mix[0]), ' ' + typeof(mix[1]), ' ' + typeof(mix[2]), ' ' + typeof(mix[3]), ' ' + typeof(mix[4]), ' ' + typeof(mix[5]), hello];
 
 for (let index = 0; index < mix.length; index++) {
     let item = mix[index]; 
--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -1,12 +1,20 @@
-// Need help on this
-
 let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
-for(let i = 0; i < myArray.length; i++ ){
-    myArray.length = myArray.length -0;
-    if(typeof variable === {});
-    hello.push(typeof(myArray))
+for(let index = 0; index < myArray.length; index++){
+    let element = myArray[index];
+    // MAGIC HAPPENS HERE
 }
 
 console.log(hello)
+
+// Need help on this
+
+// let hello = [];
+// for(let i = 0; i < myArray.length; i++ ){
+//     myArray.length = myArray.length -0;
+//     if(typeof variable === {});
+//     hello.push(typeof(myArray))
+// }
+
+// console.log(hello)
