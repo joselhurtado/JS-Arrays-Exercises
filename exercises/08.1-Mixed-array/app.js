8b989b87d240230c8c2f8100f71fc87b12b109d6
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 05:25:56 +0000

    WIP Exercise 8.2

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,13 +1,16 @@
-// Need help on this
+//Code Already Solve
 
-var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+var mix = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 
 //your code here
 let hello = {};
+hello = typeof(hello);
 
-for(let index = 0; index < mix.length; index++){
-    let item = mix[index];
-    console.log(typeof(item))
+let mixed = [typeof (mix[0]), ' ' + typeof (mix[1]), ' ' + typeof (mix[2]), ' ' + typeof (mix[3]), ' ' + typeof (mix[4]), ' ' + typeof (mix[5]), hello];
+
+for (let index = 0; index < mix.length; index++) {
+    let item = mix[index]; 
 }
 
-console.log(typeof(item))
+
+console.log(mixed);
