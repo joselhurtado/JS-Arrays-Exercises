1c52b84bb5f8333e6b20d49cbfa61671195a43b8
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 16:07:23 +0000

    Exercise 8.3

--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -1,20 +1,12 @@
+// My Way to Solve the Exercise
+
 let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
 for(let index = 0; index < myArray.length; index++){
     let element = myArray[index];
+    hello = myArray.slice(-1)
     // MAGIC HAPPENS HERE
 }
 
 console.log(hello)
-
-// Need help on this
-
-// let hello = [];
-// for(let i = 0; i < myArray.length; i++ ){
-//     myArray.length = myArray.length -0;
-//     if(typeof variable === {});
-//     hello.push(typeof(myArray))
-// }
-
-// console.log(hello)
